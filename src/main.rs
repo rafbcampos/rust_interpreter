87f358f021b0a8c52dbcfce841da7464d5d8f5da
Rafael Campos <rafael.campos@skipthedishes.ca>
2023-03-04 13:52:54 -0500

    repl

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,41 @@
 pub mod lexer;
 
+use rustyline::error::ReadlineError;
+use rustyline::{DefaultEditor, Result};
+use logos::Logos;
+use lexer::Token;
+
+
+fn repl() -> Result<()> {
+    let mut rl = DefaultEditor::new()?; 
+
+    loop {
+        let readline = rl.readline(">> ");
+        match readline {
+            Ok(line) => {
+                let tokens = Token::lexer(&line);
+                for token in tokens {
+                    println!("{:?}", token);
+                }
+            },
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break;
+            },
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break;
+            },
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
+
+    Ok(())
+}
+
 fn main() {
+    repl().unwrap();
 }
